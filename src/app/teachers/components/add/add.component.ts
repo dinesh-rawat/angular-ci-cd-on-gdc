d45316b4cc15ab73de8e75b296da1b68a9248474
DineshRawat <dineshrawat.mca@gmail.com>
2019-06-17 10:24:01 +0530

    Updated code for file upload

--- src/app/teachers/components/add/add.component.ts
+++ src/app/teachers/components/add/add.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import {HttpClient} from "@angular/common/http";
 import {MatDialogRef} from '@angular/material';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add',
@@ -14,9 +15,16 @@ export class AddComponent implements OnInit {
   educationForm: any;
   fileUploadForm: any;
 	isLinear = true;
-  files:any;
+  files = [];
+  @ViewChild('fileUpload', { read: false, static: true }) fileUpload: ElementRef;
+  chooseLabel = 'Choose';
+  uploadLabel = 'Upload';
+  cancelLabel = 'Cancel';
+  inputFileName: string;
+
   constructor(private fb: FormBuilder, private cd: ChangeDetectorRef, private http: HttpClient,
       public dialogRef: MatDialogRef<AddComponent>,
+              private sanitizer: DomSanitizer
 ) { }
 
   ngOnInit() {
@@ -68,9 +76,36 @@ export class AddComponent implements OnInit {
 	    });
 	}
 
-  onFileChange(files) {
-      this.files = files;
+  onClick() {
+    if (this.fileUpload)
+      this.fileUpload.nativeElement.click()
   }
+
+  onInput(event) {
+
+  }
+
+  onFileSelected(event) {
+    let files = event.dataTransfer ? event.dataTransfer.files : event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i];
+      file.objectURL = this.sanitizer.bypassSecurityTrustUrl((window.URL.createObjectURL(files[i])));
+      this.files.push(files[i]);
+    }
+  }
+
+  removeFile(event, file) {
+    let ix;
+    if (this.files && -1 !== (ix = this.files.indexOf(file))) {
+      this.files.splice(ix, 1)
+      this.clearInputElement()
+    }
+  }
+
+  clearInputElement() {
+    this.fileUpload.nativeElement.value = ''
+  }
+
 
   saveBasicDetails() {
   }
